04debc15c2317e8cd5124ab4dd0cf732e41bca88
Gemachis Tesfaye <gemachistesfaye36@gmail.com>
2026-09-19 23:31:30 +0300

    Add storage:link to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,5 +31,8 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+# Create storage symlink
+php artisan storage:link --force
+
 # Start the server
 exec php artisan serve --host=0.0.0.0 --port=8000
